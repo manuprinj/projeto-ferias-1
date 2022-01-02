61d040c1b2eec848ff9cea9623a920587f3e36c7
manuprinj <manuelaprinjacinto@gmail.com>
2022-01-02 16:36:56 -0300

    Rafatoração Validação CPF
    Change-Id: Ia5f16aa1a28711d19bf6e8fda70fea7b3345f435

--- src/main/java/br/com/techdive/ferias/projeto1/manuprinj/utils/CpfUtils.java
+++ src/main/java/br/com/techdive/ferias/projeto1/manuprinj/utils/CpfUtils.java
@@ -4,13 +4,15 @@ package br.com.techdive.ferias.projeto1.manuprinj.utils;
 public class CpfUtils {
 
     public static boolean isCPFValido(String cpf) {
-        if (cpf.length() != 11) return false;
+        if (cpf.equals("00000000000") || cpf.equals("11111111111") || cpf.equals("22222222222") || cpf
+                .equals("33333333333") || cpf.equals("44444444444") || cpf.equals("55555555555") || cpf
+                .equals("66666666666") || cpf.equals("77777777777") || cpf.equals("88888888888") || cpf
+                .equals("99999999999") || (cpf.length() != 11)) return false;
 
         int multiplicador = 10;
         int soma = 0;
         int digitoVerficador1;
         int digitoVerficador2;
-
 
         for (int i = 0; i < cpf.length() - 2; i++) {
             int num = (cpf.charAt(i) - 48) * multiplicador;
@@ -18,7 +20,7 @@ public class CpfUtils {
             soma += num;
         }
 
-        int resto = soma%11;
+        int resto = soma % 11;
         if (resto >= 2) {
             digitoVerficador1 = 11 - resto;
         } else {
@@ -33,7 +35,7 @@ public class CpfUtils {
             soma += num;
         }
 
-        resto = soma%11;
+        resto = soma % 11;
         if (resto >= 2) {
             digitoVerficador2 = 11 - resto;
         } else {
@@ -44,7 +46,6 @@ public class CpfUtils {
     }
 
     public static String getMascaraCPF(String cpf) {
-        return cpf.substring(0, 3) + "." + cpf.substring(3, 6) + "." + cpf.substring(6, 9)
-                + "-" + cpf.substring(9);
+        return cpf.substring(0, 3) + "." + cpf.substring(3, 6) + "." + cpf.substring(6, 9) + "-" + cpf.substring(9);
     }
 }
